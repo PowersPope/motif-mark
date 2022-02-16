a4d64267773a88d615c81e94cff407c8d341ceef
Andrew Powers <apowers4@uoregon.edu>
2022-02-15 18:50:49 -0800

    Fleshed out more of Motif class Added the logic to change the Y Pyrimidines and R purines

--- motif-mark-oopy.py
+++ motif-mark-oopy.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import cairo
+import argparse
+import itertools
 
 
 class Motif:
@@ -11,16 +13,29 @@ class Motif:
     Y Pyrimidines
     R Purines
     """
-    __init__(self, mot_string):
+    def __init__(self, mot_string):
         self.motif = mot_string.upper()
         self.purines = ['a', 'g']
         self.pyrimidines = ['c', 't']
         self.combinations = list()
-        if 'Y' in self.motif:
-            loc = [i for i, ltr in enumerate(self.motif) if ltr == 'Y']
-            for y_ent in len(loc):
-                # Need to fix this part still
-                self.combinations.append(self.motif[loc[y_ent] = 'None')
+        self.run = True
+        while self.run == True:
+            if 'Y' in self.motif
+                loc = [i for i, ltr in enumerate(self.motif) if ltr == 'Y']
+                for y_ent in loc:
+                    for ele in self.pyrimidines:
+                        self.combinations.append(self.motif[:y_ent] + ele + self.motif[y_ent+1:])
+                if 'R' not in self.motif:
+                    self.run = False
+            if 'R' in self.motif:
+                loc = [i for i, ltr in enumerate(self.motif) if ltr == 'R']
+                for r_ent in loc:
+                    for ele in self.purines:
+                        self.combinations.append(self.motif[:r_ent] + ele + self.motif[r_ent+1:])
+            else:
+                self.combinations.append(self.motif)
+
+
 class Gene:
     """
     Add in Exons and intron information
