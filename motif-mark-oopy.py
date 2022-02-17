79c4459044ab82a3eaec928af5617d59b8de7fa6
Andrew Powers <apowers4@uoregon.edu>
2022-02-17 00:11:17 -0800

    Finished the Cairo class Writes out all genes and motifs with specific colors associated with it Made a legend for the Motifs and colors associated Added header above each fasta line/gene graph

--- motif-mark-oopy.py
+++ motif-mark-oopy.py
@@ -4,6 +4,7 @@ import cairo
 import argparse
 import itertools as it
 import re
+import random
 
 
 def get_args():
@@ -54,7 +55,6 @@ class Motif:
         # grab the length of each item
         pre_len = len(pre)
         exon_len = len(exon)
-        post_len = len(post)
         preAndexon = exon_len + pre_len
         # Find the particular items of interest
         pre_matches = re.finditer(self.motif_regex, pre)
@@ -115,9 +115,95 @@ class Cairo:
     Take in the Motif and Gene object and then draw it.
     """
 
-    def __init__(self, gene_dict):
-        self.width = max([gene_dict[n].gene_len for n in gene_dict]) + 200
-        self.height = 600
+    def __init__(self, gene_dict, motif_dict):
+        self.width = max([gene_dict[n].gene_len for n in gene_dict]) + 100
+        self.height = 500
+        self.genes = gene_dict
+        self.motif = motif_dict
+        self.color_motif = dict()
+        random.seed(10)
+        for mot in motif_dict:
+            self.color_motif[mot] = [round(random.random(), 2) for _ in range(0, 3)]
+
+    def graph_data(self):
+        # Set up the canvas
+        surface = cairo.ImageSurface(cairo.FORMAT_RGB24, self.width, self.height)
+        context = cairo.Context(surface)
+
+        # Set background color white
+        context.save()
+        context.set_source_rgb(1, 1, 1)
+        context.rectangle(0, 0, self.width, self.height)
+        context.fill()
+
+        x = 30
+        y = 100
+        for ent in gene_dict:
+            context.set_source_rgb(0, 0, 0)
+            context.set_line_width(3)
+            context.set_line_cap(cairo.LINE_CAP_BUTT)
+            context.move_to(x, y)  # (x, y)
+            context.line_to(gene_dict[ent].gene_len + x, y)
+            context.close_path()
+            context.stroke()
+
+            # Draw the gene
+            context.rectangle(
+                len(gene_dict[ent].pre_exon) + x,
+                y - (40 / 2),
+                len(gene_dict[ent].exon),
+                40,
+            )
+            context.fill()
+
+            # Draw motifs
+            for motif in gene_dict[ent].show_matches():
+                for reg_mot in gene_dict[ent].show_matches()[motif]:
+                    col1 = self.color_motif[motif][0]
+                    col2 = self.color_motif[motif][1]
+                    col3 = self.color_motif[motif][2]
+                    for span in gene_dict[ent].show_matches()[motif][reg_mot]:
+                        xstart = span[0] + x
+                        xend = span[1] + x
+                        context.set_source_rgb(col1, col2, col3)
+                        context.rectangle(xstart, y - (40 / 2), xend - xstart, 40)
+                        context.fill()
+
+            # Draw header
+            context.set_source_rgb(0, 0, 0)
+            context.select_font_face(
+                "Courier", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD
+            )
+            context.set_font_size(15)
+            context.move_to(50, y - 30)
+            context.show_text(gene_dict[ent].header)
+
+            # Go to the next one
+            y += 100
+
+        x = self.width - 150
+        y = 70
+        for mot_text in self.color_motif:
+            # Make color show
+            context.set_source_rgb(
+                self.color_motif[mot_text][0],
+                self.color_motif[mot_text][1],
+                self.color_motif[mot_text][2],
+            )
+            context.set_line_width(15)
+            context.move_to(x, y)
+            context.line_to(x + 5, y)
+            context.stroke()
+            # Write motif legend
+            context.move_to(x + 10, y + 5)
+            context.select_font_face(
+                "Courier", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD
+            )
+            context.set_font_size(20)
+            context.show_text(mot_text.lower())
+            y += 20
+
+        surface.write_to_png("test.png")
 
 
 ####### Logic
@@ -166,11 +252,14 @@ with open(args.file, "r") as file:
     entry_holder.append(unwrapped_line)
     gene_dict[num] = Gene(entry_holder, motif_dict)
 
-# print(gene_dict)
-for num in range(1, len(gene_dict) + 1):
-    print(gene_dict[num].show_matches())
-
-# for k in gene_dict[1].show_matches():
-# for i in gene_dict[1].show_matches()[k]:
+# Print data
+# for num in range(1, len(gene_dict) + 1):
+# for i in gene_dict[num].show_matches():
 # print(i)
+# for k in gene_dict[num].show_matches()[i]:
+# print(k)
+# print(gene_dict[num].show_matches()[i][k])
 
+test = Cairo(gene_dict, motif_dict)
+print(test.color_motif)
+test.graph_data()
